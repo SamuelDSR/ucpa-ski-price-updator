770b5f2e24b5179aab14fb3a1eace383d8cffb4d
samuel <samuel@adomik.com>
2019-09-03 21:36:57 +0200

    [bug]fix issues when elements not found

--- crawler.py
+++ crawler.py
@@ -1,36 +1,45 @@
 import json
 
 from selenium import webdriver
-from selenium.webdriver import FirefoxOptions
+from selenium.webdriver import FirefoxOptions, ChromeOptions
 
-options = FirefoxOptions()
+#  options = FirefoxOptions()
+options = ChromeOptions()
 options.add_argument("--headless")
-driver = webdriver.Firefox(options=options)
+#  driver = webdriver.Firefox(options=options)
+driver = webdriver.Chrome("./chromedriver", options=options)
 
 # search start url
 start_url = "https://www.ucpa.com/activite/ski-alpin"
-driver.get(start_url)
 
-resort_urls = [
-    x.find_element_by_tag_name("a").get_attribute("href")
-    for x in driver.find_elements_by_css_selector(
+
+def get_resort_url_list(start_url):
+    driver.get(start_url)
+    resort_webitems = driver.find_elements_by_css_selector(
         ".ucpa-Search-item.ucpa-Search-item--simple")
-]
+    urls = []
+    for item in resort_webitems:
+        elements = item.find_elements_by_tag_name("a")
+        if elements:
+            urls.append(elements[0].get_attribute("href"))
+    return urls
 
 
 def get_products_by_resort(url):
     driver.get(url)
-    product_grids = driver.find_elements_by_css_selector(
-        "r-Grid.ucpa-Product-list")
-    all_offers = []
-    for grid in product_grids:
-        offer = {}
-        offer["url"] = grid.find_element_by_tag_name("a").get_attribute("href")
-        offer_details = json.loads(
-            grid.find_element_by_tag_name("a").get_attribute("analytics-data"))
-        offer.update(offer_details)
-        all_offers.append(offer)
-    return all_offers
+    product_webitems = driver.find_elements_by_css_selector(
+        ".r-Grid.ucpa-Product-list")
+    all_products = []
+    for grid in product_webitems:
+        product = {}
+        a_webelem = grid.find_elements_by_tag_name("a")
+        if a_webelem:
+            product["url"] = a_webelem[0].get_attribute("href")
+            product_details = json.loads(
+                a_webelem[0].get_attribute("analytics-data"))
+            product.update(product_details)
+            all_products.append(product)
+    return all_products
 
 
 def get_sejours_by_product(product_url):
@@ -41,20 +50,42 @@ def get_sejours_by_product(product_url):
     ]
 
     def parse_sejour(web_element):
-        sejour_detail = {}
+        sejour = {}
         for prop in target_props:
-            sejour_detail[prop] = web_element.get_attribute(prop)
+            sejour[prop] = web_element.get_attribute(prop)
 
-        sejour_detail["reduction"] = web_element.find_element_by_class_name(
-            "ucpa-Sejour-result-date").get_attribute("data-reduc")
-        sejour_detail["price_unity"] = web_element.find_element_by_class_name(
-            "ucpa-Sejour-result-oldPrice").get_attribute("innerHTML")
-        sejour_detail["duration"] = web_element.find_element_by_class_name(
-            "ucpa-Sejour-result-during").get_attribute("innerHTML")
-        return sejour_detail
+        date_items = web_element.find_elements_by_class_name(
+            "ucpa-Sejour-result-date")
+        if date_items:
+            sejour["reduction"] = date_items[0].get_attribute("data-reduc")
 
-    all_sejour_details = [
-        parse_sejour(x)
-        for x in driver.find_elements_by_css_selector("ucpa-Sejour-resultItem")
+        price_unity_items = web_element.find_elements_by_class_name(
+            "ucpa-Sejour-result-oldPrice")
+        if price_unity_items:
+            sejour["price_unity"] = price_unity_items[0].get_attribute("innerHTML")
+
+        during_items = web_element.find_elements_by_class_name(
+            "ucpa-Sejour-result-during")
+        if during_items:
+            sejour["duration"] = during_items[0].get_attribute("innerHTML")
+
+        return sejour
+
+    all_sejours = [
+        parse_sejour(x) for x in driver.find_elements_by_css_selector(
+            ".ucpa-Sejour-resultItem")
     ]
-    return all_sejour_details
+    return all_sejours
+
+
+resort_urls = get_resort_url_list(start_url)
+resort_urls = resort_urls[:2]
+sejour_list = []
+for url in resort_urls:
+    for product in get_products_by_resort(url):
+        sejs = get_sejours_by_product(product["url"])
+        for s in sejs:
+            s.update(product)
+        sejour_list.extend(sejs)
+
+json.dump(sejour_list, open("ucpa-offer-list.json", "w"))
